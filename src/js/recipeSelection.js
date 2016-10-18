bab27260425cbe28261bc9058a49e065a2b82618
Nino Küenzi <nino.kueenzi@contexta.ch>
2016-10-18 09:53:45 +0200

    bug fix touch devices 2

--- src/js/recipeSelection.js
+++ src/js/recipeSelection.js
@@ -1,31 +1,12 @@
-if(!is_touch_device){
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+}else{
     $(".teaser-image").hover(function(){
         $(this).stop().addClass("onhover");
     }, function(){
         $(this).stop().removeClass("onhover");
     });
 }
-
-var is_touch_device = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-};
 
 var selectedRecept = 0;
 
