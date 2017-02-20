bddf35b41ec295fd72b1bb21101446d722e8eb63
Nino Luca <ninokueenzi@gmail.com>
2017-02-21 00:12:16 +0100

    work in progress

--- src/js/onload.js
+++ src/js/onload.js
@@ -232,14 +232,18 @@ $(document).ready(function(){
     function createPicker() {
         if (pickerApiLoaded && oauthToken) {
             var view = new google.picker.View(google.picker.ViewId.DOCS_IMAGES);
-            // view.setMimeTypes("image/png,image/jpeg,image/jpg");
             view.setParent("0B_087vROMGnrMVYtZExpaUlDaGs");
+
+            var uploadView = new google.picker.DocsUploadView();
+            uploadView.setParent("0B_087vROMGnrMVYtZExpaUlDaGs");
+
             var picker = new google.picker.PickerBuilder()
                 // .enableFeature(google.picker.Feature.NAV_HIDDEN)
-                .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+                //.enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
                 .setAppId(appId)
                 .setOAuthToken(oauthToken)
                 .addView(view)
+                .addView(uploadView)
                 // .addView(new google.picker.DocsUploadView())
                 .setDeveloperKey(developerKey)
                 .setCallback(pickerCallback)
